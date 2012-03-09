d517d95cf1f79703d98adbf20f7d3c3aa6ff4c14
Bill Dueber <bill@dueber.com>
2012-03-09 08:05:39 -0500

    Added names stuff

---
+++ ruby/names_query.rb
@@ -0,0 +1,7 @@
+{
+  'fl' => 'score, *',
+  'defType' => 'dismax',
+  'wt' => 'csv',
+  'q' => 'davy jones',
+  'qf' => 'name_text'
+}
---
+++ ruby/names_sloppy_query.rb
@@ -0,0 +1,9 @@
+{
+  'fl' => 'score, *',
+  'defType' => 'dismax',
+  'wt' => 'csv',
+  'q' => 'davy jones',
+  'qf' => 'name_text',
+  'pf' => 'name_text^10',
+  'ps' => '4'
+}
