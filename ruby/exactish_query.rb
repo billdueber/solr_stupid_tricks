c038ca156c5066d3c6cd38829617eb6f597f946e
Bill Dueber <bill@dueber.com>
2012-03-19 23:22:56 -0400

    Cleanup for exactish

--- ruby/exactish_query.rb
+++ ruby/exactish_query.rb
@@ -4,21 +4,21 @@
   'defType' => 'dismax',
   'q' => 'the monkees',
   'qf' => 'title',
-  'pf' => 'title'
+  'pf' => 'title^2'
   },
   {
   'fl' => 'score, *',
   'defType' => 'dismax',
   'q' => 'the monkees',
   'qf' => 'title',
-  'pf' => 'title_exact^10 title'
+  'pf' => 'title_exact^10 title^2'
   },
   {
   'fl' => 'score, *',
   'defType' => 'dismax',
   'q' => 'the monkees',
   'qf' => 'title',
-  'pf' => 'title_exact^10 title_l^3 title'
+  'pf' => 'title_exact^10 title_l^3 title^2'
   }
 
 ]
